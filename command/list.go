d90168a989ccb2b29eecd9c10271f01727c594b0
Chris McEniry <cmceniry@mit.edu>
2015-12-03 16:33:51 -0800

    * cleanup: dont use new var unless needed

--- command/list.go
+++ command/list.go
@@ -9,9 +9,7 @@ import (
 func List(opts *GeneralOptions, args []string) {
 	opts.Stash.Load()
 
-	s := opts.Stash
-
-	archivenames := s.GetArchivelist()
+	archivenames := opts.Stash.GetArchivelist()
 	sort.Strings(archivenames)
 	for _, a := range archivenames {
 		fmt.Printf("%s\n", a)
